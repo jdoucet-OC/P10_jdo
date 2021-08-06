5a2599d6d86e1e175d57ffa3425708c63c99f9b5
unknown <jdoucetoc@gmail.com>
2021-08-06 11:11:19 +0200

    Hotfix PEP8

--- ocapi/softdesk/urls.py
+++ ocapi/softdesk/urls.py
@@ -11,12 +11,18 @@ from . import views
 
 router = routers.SimpleRouter()
 router.register(r'projects', views.ProjectsViewSet)
-contributor_router = routers.NestedSimpleRouter(router, r'projects', lookup='project')
-contributor_router.register(r'users', views.ContributorsViewSet, basename='contributors')
-issue_router = routers.NestedSimpleRouter(router, r'projects', lookup='project')
-issue_router.register(r'issues', views.IssuesViewSet, basename='issues')
-comment_router = routers.NestedSimpleRouter(issue_router, r'issues', lookup='issues')
-comment_router.register(r'comments', views.CommentsViewSet, basename='comments')
+contributor_router = routers.NestedSimpleRouter(router, r'projects',
+                                                lookup='project')
+contributor_router.register(r'users', views.ContributorsViewSet,
+                            basename='contributors')
+issue_router = routers.NestedSimpleRouter(router, r'projects',
+                                          lookup='project')
+issue_router.register(r'issues', views.IssuesViewSet,
+                      basename='issues')
+comment_router = routers.NestedSimpleRouter(issue_router,
+                                            r'issues', lookup='issues')
+comment_router.register(r'comments', views.CommentsViewSet,
+                        basename='comments')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -25,12 +31,17 @@ urlpatterns = [
     path('', include(contributor_router.urls)),
     path('', include(issue_router.urls)),
     path('', include(comment_router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signup/', views.RegisterUser().as_view(), name='register_user'),
+    path('api-auth/', include('rest_framework.urls',
+                              namespace='rest_framework')),
+    path('login/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('signup/', views.RegisterUser().as_view(),
+         name='register_user'),
 
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
